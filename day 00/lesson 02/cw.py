dde3d7958ab9da5296aa5778b0c1f4904bad23c0
canicani121 <aleqsandrebabaev@gmail.com>
2025-10-17 20:36:27 +0400

    second commit

---
+++ day 00/lesson 02/cw.py
@@ -0,0 +1,24 @@
+#1.
+#a = "gamarjoba"
+#print(a)
+
+#3.
+a= 10
+b= 2
+print(a + b)   #12
+print(a - b)   #8
+print(a * b)   #20
+print(a / b)   #5.0 ან #5
+
+#4.
+print(" alex " * 20)
+
+#6.
+name= "student"
+print(name + name)
+number= 20               #ერთმანეთს დაუმატებს რაც number=20 number1=4 20+4=24
+number1= 4
+print(number + number1)
+
+#10.
+#01101000 01100101 01101100 01101100 01101111
