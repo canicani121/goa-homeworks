b981a43d77ec7d3953bc7e548077d53868be49c6
canicani121 <aleqsandrebabaev@gmail.com>
2025-10-24 18:43:33 +0400

    commit

---
+++ day 02/classwork/classwork.py
@@ -0,0 +1,30 @@
+# ცვლადი არის იგივე ყუთი სადაც სხვადასხვა მონაცემის შენახვა შეგვიძლია
+
+
+food = "pizza"
+
+
+print("alex")
+print(12)
+print(3.4)
+
+
+name = "alex"
+name = "lasha"
+print(name)
+
+
+print("alex " + "babaevi")
+
+
+print("house" * 3) # househousehouse
+
+
+print("this is string")
+surname = "your surname"
+car = "mercedes"
+
+
+lesson = "math"
+print("lesson") # არ გამოიტანს math-ს რადგან lesson-ი არის string-ის სახით
+print(lesson)
