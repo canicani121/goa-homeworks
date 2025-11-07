3ce767df5c2e436e6b39cae19ffeaaacd7982c73
canicani121 <aleqsandrebabaev@gmail.com>
2025-11-07 23:22:19 +0400

    commit

---
+++ day 05/classwork.py
@@ -0,0 +1,34 @@
+
+
+age = 13
+age = 14
+print(age)
+
+
+name = "giorgi"
+Name = "vano"
+print(name)  # გამოიტანს giorgi
+print(Name)  # გამოიტანს vano
+
+
+num1 = 5
+num2 = 7
+product = num1 * num2
+print(product)
+
+
+print(7634 % 4)   # 2
+print(982 % 3)    # 2
+print(12345 % 3)  # 0
+
+
+#giorgi, nika
+
+
+age = 23
+print(age)
+
+num = 34
+print(num + 5)
+
+print("5" * 3)  # გამოიტანს 555
---
+++ day 05/homework.py
@@ -0,0 +1,21 @@
+# debugging პითონში გულისხმობს კოდში შეცდომების პოვნას და გამოსწორებას. 
+# name=13
+# name=14
+# print(name)
+
+
+first_name=("alex")
+last_name=("babaev")
+
+full_name=first_name + " " + last_name
+print(full_name)
+
+
+name=("sali")
+name=("batu")
+print(name)
+
+
+num = "40"
+print(num * 3)
+# Write to alex babaev
