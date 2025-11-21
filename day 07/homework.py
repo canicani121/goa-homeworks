fc08f46284f8c5494fd517a965399d746c169052
canicani121 <aleqsandrebabaev@gmail.com>
2025-11-22 00:01:13 +0400

    commit

---
+++ day 07/homework.py
@@ -0,0 +1,26 @@
+age = int(input("შეიყვანე შენი ასაკი: "))
+result = age // 10
+print("რიცხვი 10 მოთავსდება შენს ასაკში:", result, "-ჯერ")
+
+
+num1 = float(input("შეიყვანე num1: "))
+num2 = float(input("შეიყვანე num2: "))
+print("ნამრავლი არის:", num1 * num2)
+
+
+age = input("შეიყვანე შენი ასაკი: ")
+age = int(age)
+print("ასაკი ინტეჯერად:", age)
+
+
+num = float(input("num: "))
+num1 = float(input("num1: "))
+num2 = float(input("num2: "))
+avg = (num + num1 + num2) / 3
+print("საშუალო არითმეტიკული:", avg)
+
+
+birth_year = int(input("შეიყვანე შენი დაბადების წელი: "))
+current_year = 2025
+age = current_year - birth_year
+print("შენი ასაკია:", age)
