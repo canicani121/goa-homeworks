58156b4b5d13db3cdde5b3bfd894913997caf4bd
canicani121 <aleqsandrebabaev@gmail.com>
2025-12-06 00:21:44 +0400

    commit

---
+++ day 09/classwork.py
@@ -0,0 +1,23 @@
+# explicit → როცა თვითონ ვცვლით ტიპს (int(), float(), str())
+# implicit → Python თვითონ ცვლის ტიპს
+
+
+# int(5.7)
+# float(10)
+# str(123)
+# bool(1)
+
+
+num = 6/2
+print(type(num))
+# <class 'float'>
+
+
+number = 6.0
+number > 6
+# False
+
+
+a = int(input())
+b = int(input())
+print(a // b)   # ინტეჯერის გაყოფა
