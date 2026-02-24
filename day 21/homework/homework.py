12f6770e034be4739091309fcab76be3b56c0d29
canicani121 <aleqsandrebabaev@gmail.com>
2026-02-24 22:46:09 +0400

    commit

---
+++ day 21/homework/homework.py
@@ -0,0 +1,19 @@
+print("Hello")
+print(10)
+print("Hi", "Alex")
+
+input("შეიყვანე სახელი: ")
+input("რამდენი წლის ხარ? ")
+input("შეიყვანე რიცხვი: ")
+
+int("5")
+int("100")
+int("25")
+
+str(10)
+str(50)
+str(123)
+
+float("5.5")
+float("10")
+float("3.14")
